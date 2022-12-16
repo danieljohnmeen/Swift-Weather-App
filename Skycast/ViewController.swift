0b161e361a7ff321385c952e8dfc3f5e60ccbab0
danieljohnmeen <danijomeen@gmail.com>
2022-12-16 20:51:24 +0300

    Initial Commit

---
+++ Skycast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Skycast
+//
+//  Created by Малиль Дугулюбгов on 16.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
